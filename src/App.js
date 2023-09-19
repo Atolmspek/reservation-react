f7a9f8fefd273add41e941682598176fb7c3c87d
Martin Antelo <martin.docenciacip@gmail.com>
2023-09-19 22:20:43 +0200

    more work, still missing stuff

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Forms from "./components/Forms";
 
 function App() {
+  //localStorage.clear();
 
-  function handleSubmitSuccess() {
-    alert('Formulario enviado con éxito. Realizar acciones adicionales aquí.');
-    // Realiza las acciones adicionales que desees en el componente principal
-  }
+    // Recuperar datos de reservas almacenados en el localStorage al inicio
+    const initialReservations = JSON.parse(localStorage.getItem("reservationData")) || [];
+    const [reservations, setReservations] = useState(initialReservations);
+  
+    function areObjectsEqual(objA, objB) {
+      const stringA = JSON.stringify(objA);
+      const stringB = JSON.stringify(objB);
+      return stringA === stringB;
+    }
+    
+    // ...
+    
+    function handleSubmitSuccess(date, timeSlot, name, surname) {
+      const storedData = localStorage.getItem("reservationData");
+      if (storedData) {
+        const parsedData = JSON.parse(storedData);
+        console.log("Stored Data:", parsedData);
+        console.log("Form Data:", { date, timeSlot, name, surname });
+    
+        // Verificar si la nueva reserva coincide con alguna reserva existente
+        const isDuplicate = parsedData.some((reservation) =>
+          areObjectsEqual(reservation, { date, timeSlot, name, surname })
+        );
+    
+        if (isDuplicate) {
+          alert("Reservation already made with that name and time");
+          return;
+        }
+      }
+    
+      const newReservation = {
+        date,
+        timeSlot,
+        name,
+        surname,
+      };
+    
+      // Agregar la nueva reserva al array de reservas existente
+      const updatedReservations = [...parsedData, newReservation];
+    
+      // Guardar el array actualizado en el localStorage
+      localStorage.setItem("reservationData", JSON.stringify(updatedReservations));
+    
+      // Actualizar el estado con el nuevo array de reservas
+      setReservations(updatedReservations);
+    }
 
   return (
-    <Forms onSubmitSuccess={handleSubmitSuccess} />
+    <Forms
+      onSubmitSuccess={handleSubmitSuccess}
+     
+    />
   );
-}
+  }
 
 export default App;
--- src/components/Forms.js
+++ src/components/Forms.js
@@ -1,85 +1,107 @@
 import React, { useState } from "react";
 import {
-  ChakraProvider,
-  FormControl,
-  FormLabel,
-  Select,
-  Input,
-  Button,
-  Box,
-  Text,
+    ChakraProvider,
+    FormControl,
+    FormLabel,
+    Select,
+    Input,
+    Button,
+    Box,
+    Text,
 } from "@chakra-ui/react";
 import { SingleDatepicker } from "chakra-dayzed-datepicker";
 
 export default function Forms(props) {
-  const [date, setDate] = useState(new Date());
-  const [name, setName] = useState("");
-  const [surname, setSurname] = useState("");
-  const [dateError, setDateError] = useState("");
-  const [nameError, setNameError] = useState("");
-  const [surnameError, setSurnameError] = useState("");
+    const [date, setDate] = useState(new Date());
+    const [name, setName] = useState("");
+    const [surname, setSurname] = useState("");
+    const [dateError, setDateError] = useState("");
+    const [nameError, setNameError] = useState("");
+    const [surnameError, setSurnameError] = useState("");
+    const [timeSlot, setTimeSlot] = useState(""); // Estado para el campo de selección de hora
+    const [timeSlotError, setTimeSlotError] = useState(""); // Estado para el mensaje de error del campo de selección de hora
 
-  const handleSubmit = (event) => {
-    event.preventDefault(); 
-    
-    setDateError("");
-    setNameError("");
-    setSurnameError("");
+    const handleSubmit = (event) => {
+        event.preventDefault();
 
-    //Field validatio
+        setDateError("");
+        setNameError("");
+        setSurnameError("");
+        setTimeSlotError("");
 
-    if (!name.trim()) {
-      setNameError("Name is required");
-      return;
-    }
 
-    if (!surname.trim()) {
-      setSurnameError("Surname is required");
-      return;
-    }
+        if (!timeSlot) {
+            setTimeSlotError("Please select a time slot");
+            return;
+        }
 
-    props.onSubmitSuccess();
-  };
 
-  return (
-    <ChakraProvider>
-      <Box p={4} maxWidth="700px" margin="0 auto">
-        <form onSubmit={handleSubmit}>
-          <FormControl mb={4}>
-            <FormLabel fontWeight="bold">Date</FormLabel>
-            <SingleDatepicker
-              name="date-input"
-              date={date}
-              onDateChange={setDate}
-            />
-            {dateError && <Text color="red">{dateError}</Text>}
-          </FormControl>
+        if (!name.trim()) {
+            setNameError("Name is required");
+            return;
+        }
 
-          <FormControl mb={4}>
-            <FormLabel fontWeight="bold">Name</FormLabel>
-            <Input
-              placeholder="Your name"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            {nameError && <Text color="red">{nameError}</Text>}
-          </FormControl>
+        if (!surname.trim()) {
+            setSurnameError("Surname is required");
+            return;
+        }
 
-          <FormControl mb={4}>
-            <FormLabel fontWeight="bold">Your surname</FormLabel>
-            <Input
-              placeholder="Your surname"
-              value={surname}
-              onChange={(e) => setSurname(e.target.value)}
-            />
-            {surnameError && <Text color="red">{surnameError}</Text>}
-          </FormControl>
+        props.onSubmitSuccess(date, timeSlot, name, surname );
+    };
 
-          <Button colorScheme="blue" type="submit">
-            Submit
-          </Button>
-        </form>
-      </Box>
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider>
+            <Box p={4} maxWidth="700px" margin="0 auto">
+                <form onSubmit={handleSubmit}>
+                    <FormControl mb={4}>
+                        <FormLabel fontWeight="bold">Date</FormLabel>
+                        <SingleDatepicker
+                            name="date-input"
+                            date={date}
+                            onDateChange={setDate}
+                        />
+                        {dateError && <Text color="red">{dateError}</Text>}
+                    </FormControl>
+
+                    <FormControl mb={4}>
+                        <FormLabel fontWeight="bold">Time slot</FormLabel>
+                        <Select
+                            placeholder="Select option"
+                            value={timeSlot}
+                            onChange={(e) => setTimeSlot(e.target.value)}
+                           
+                        >
+
+                            <option value="8:00 AM">8:00 AM</option>
+                            <option value="9:00 AM">9:00 AM</option>
+                            <option value="10:00 AM">10:00 AM</option>
+                        </Select>
+                        {timeSlotError && <Text color="red">{timeSlotError}</Text>}
+                    </FormControl>
+
+                    <FormControl mb={4}>
+                        <FormLabel fontWeight="bold">Name</FormLabel>
+                        <Input
+                            placeholder="Your name"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                        {nameError && <Text color="red">{nameError}</Text>}
+                    </FormControl>
+
+                    <FormControl mb={4}>
+                        <FormLabel fontWeight="bold">Surname</FormLabel>
+                        <Input
+                            placeholder="Your surname"
+                            value={surname}
+                            onChange={(e) => setSurname(e.target.value)}
+                        />
+                    </FormControl>
+                    <Button colorScheme="blue" type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </Box>
+        </ChakraProvider>
+    );
 }
